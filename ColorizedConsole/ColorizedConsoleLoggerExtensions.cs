037ff08cad2daa62349007083c197e721f1984c0
hishamco <hishamco_2007@yahoo.com>
2017-01-30 23:35:39 +0300

    Add 'AddColorizedConsole' extension method

---
+++ ColorizedConsole/ColorizedConsoleLoggerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging.ColorizedConsole;
+using Microsoft.Extensions.Logging.ColorizedConsole.Classification;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.Logging
+{
+    public static class ColorizedConsoleLoggerExtensions
+    {
+        public static ILoggerFactory AddColorizedConsole(this ILoggerFactory factory, RegexClassification[] classifications)
+        {
+            factory.AddProvider(new ColorizedConsoleLoggerProvider((n, l) => l >= LogLevel.Information, false, classifications));
+            return factory;
+        }
+    }
+}
